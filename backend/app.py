f3d943aa8cae04f68f77a6f480e5e1d6dade50d9
logenpark <logen.park@dgmit.com>
2025-07-15 18:45:48 +0900

    chore: 불필요한 코드를 삭제한다

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,6 @@
 from config import config
 from flask import Flask
-from mysql_connection import (
-    ensure_tables_exist,
-    get_all_users,
-    get_connection,
-    insert_user,
-    select_user_by_email,
-)
+from mysql_connection import ensure_tables_exist, get_connection
 
 app = Flask(__name__)
 
@@ -22,10 +16,6 @@ if __name__ == "__main__":
     if cnx:
         print(cnx)
         ensure_tables_exist(cnx)
-        get_all_users(cnx)
-        insert_user("chec@naver.com", "qweqweqwe", "asda", cnx)
-        select_user_by_email("chec@naver.com", cnx)
-        cnx.close()
     else:
         print("DB 연결 실패")
     app.run()
