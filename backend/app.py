6504b7cabf6a48a9cbb448afe481534ae176f87f
logenpark <logen.park@dgmit.com>
2025-07-16 09:12:09 +0900

    feat: 사용자 및 할일 관련 기본 API 엔드포인트 라우팅을 추가한다

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,54 @@
 from config import config
-from flask import Flask
+from flask import Flask, request, jsonify
 from mysql_connection import ensure_tables_exist, get_connection
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "Hello World!"
+@app.route("/login", methods=["POST"])
+def login():
+    return jsonify({"message": "로그인 성공"})
+
+@app.route("/register", methods=["POST"])
+def register():
+    return jsonify({"message": "회원가입 성공"})
+
+@app.route("/logout", methods=["POST"])
+def logout():
+    return jsonify({"message": "로그아웃 성공"})
+
+@app.route("/withdraw", methods=["POST"])
+def withdraw():
+    return jsonify({"message": "회원탈퇴 성공"})
+
+@app.route("/user", methods=["GET"])
+def info_me():
+    return jsonify({"message": "유저 본인정보 조회 성공"})
+
+@app.route("/todos", methods=["POST"])
+def add_todo():
+    return jsonify({"message": "할일 등록 성공"})
+
+@app.route("/todos/<int:id>", methods=["GET"])
+def get_todo(id):
+    return jsonify({"message": f"{id}번째 할일 조회 성공"})
+
+@app.route("/todos", methods=["GET"])
+def get_todos():
+    return jsonify({"message": "할일 목록조회 성공"})
+
+@app.route("/todos/<int:id>", methods=["PUT"])
+def modify_todo(id):
+    return jsonify({"message": f"{id}번째 할일 수정 성공"})
+
+
+@app.route("/todos/<int:id>", methods=["DELETE"])
+def delete_todo(id):
+    return jsonify({"message": f"{id}번째 할일 삭제 성공"})
+
+@app.route("/todos/<int:id>/toggle", methods=["PATCH"])
+def toggle_todo(id):
+    return jsonify({"message": f"{id}번째 할일 완료 상태 변경 성공"})
 
 
 if __name__ == "__main__":
