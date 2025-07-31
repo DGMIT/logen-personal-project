54e8d7e66e622a3d18b9c888323b1ec6d7a29947
logenpark <logen.park@dgmit.com>
2025-07-31 22:00:49 +0900

    feat: error별 테스트코드를 작성한다

--- apps/api/test/sample_utils.py
+++ apps/api/test/sample_utils.py
@@ -1,3 +1,6 @@
+from fastapi import HTTPException
+
+
 def inc(x: int) -> int:
     return x + 1
 
@@ -29,3 +32,39 @@ def is_min_length(s: str, min_len: int) -> bool:
     if len(s.strip()) >= min_len:
         return True
     return False
+
+
+def raise_value_error_function():
+    raise ValueError("ValueError 에러 발생")
+
+
+def raise_type_error_function():
+    raise TypeError("TypeError 에러 발생")
+
+
+def raise_zero_division__error_function():
+    raise ZeroDivisionError("ZeroDivisionError 에러 발생")
+
+
+def raise_key_error_function():
+    raise KeyError("KeyError 에러 발생")
+
+
+def raise_index_error_function():
+    raise IndexError("IndexError 에러 발생")
+
+
+def raise_attribute_error_function():
+    raise AttributeError("AttributeError 에러 발생")
+
+
+def raise_file_not_found_error_function():
+    raise FileNotFoundError("FileNotFoundError 에러 발생")
+
+
+def raise_assertion_error_function():
+    raise AssertionError("AssertionError 에러 발생")
+
+
+def raise_httpexception_error_function():
+    raise HTTPException(status_code=400, detail="400")
--- apps/api/test/test_sample.py
+++ apps/api/test/test_sample.py
@@ -1,4 +1,6 @@
+import pytest
 import sample_utils
+from fastapi import HTTPException
 
 
 def test_numbers_are_equal():
@@ -83,3 +85,59 @@ class TestInspectStringLenGroup:
         assert (
             sample_utils.is_min_length(self.inner_gap_example, self.min_length) == True
         )
+
+
+class TestCommonErrorCases:
+    # 1. ValueError
+    # 어떤 함수에 음수를 넣었을 때 ValueError가 발생해야 한다.
+    def test_raise_value_error(self):
+        with pytest.raises(ValueError):
+            sample_utils.raise_value_error_function()
+
+    # 2. TypeError
+    # 숫자가 들어가야 할 자리에 문자열을 넣었을 때 TypeError가 발생해야 한다.
+    def test_raise_type_error(self):
+        with pytest.raises(TypeError):
+            sample_utils.raise_type_error_function()
+
+    # 3. ZeroDivisionError
+    # 0으로 나누려고 할 때 ZeroDivisionError가 발생해야 한다
+    def test_raise_zero_division_error(self):
+        with pytest.raises(ZeroDivisionError):
+            sample_utils.raise_zero_division__error_function()
+
+    # 4. KeyError
+    # 딕셔너리에 없는 키를 조회할 때 KeyError가 발생해야 한다.
+    def test_raise_key_error(self):
+        with pytest.raises(KeyError):
+            sample_utils.raise_key_error_function()
+
+    # 5. IndexError
+    # 리스트의 존재하지 않는 인덱스를 조회할 때 IndexError가 발생해야 한다.
+    def test_raise_index_error(self):
+        with pytest.raises(IndexError):
+            sample_utils.raise_index_error_function()
+
+    # 6. AttributeError
+    # 존재하지 않는 속성에 접근할 때 AttributeError가 발생해야 한다.
+    def test_raise_attribute_error(self):
+        with pytest.raises(AttributeError):
+            sample_utils.raise_attribute_error_function()
+
+    # 7. FileNotFoundError
+    # 존재하지 않는 파일을 열려고 할 때 FileNotFoundError가 발생해야 한다.
+    def test_raise_file_not_found_error(self):
+        with pytest.raises(FileNotFoundError):
+            sample_utils.raise_file_not_found_error_function()
+
+    # 8. AssertionError
+    # assert 조건이 거짓일 때 AssertionError가 발생해야 한다.
+    def test_raise_assertion_error(self):
+        with pytest.raises(AssertionError):
+            sample_utils.raise_assertion_error_function()
+
+    # 9. HTTPException (FastAPI)
+    # 인증 없이 보호된 라우터에 접근할 때 HTTPException (401 또는 403)이 발생해야 한다.
+    def test_raise_htttpexception_error(self):
+        with pytest.raises(HTTPException):
+            sample_utils.raise_httpexception_error_function()
